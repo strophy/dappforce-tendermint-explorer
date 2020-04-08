c82d27bbf1cf862c19cb673d8ba002faf2eef3e5
cloudWheels <44451128+cloudwheels@users.noreply.github.com>
2020-04-08 01:09:23 +0100

    Update tx.js

--- src/scripts/tx.js
+++ src/scripts/tx.js
@@ -1,5 +1,4 @@
 import b64 from "base64-js"
-import * as cbor from "../assets/js/cbor.js"
 
 // See https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
 export const encodeBase64 = (str, encoding = 'utf-8') => {
@@ -18,8 +17,4 @@ export const decodeTx = (base64str) => {
   let idx = str.indexOf('{')
   let json = str.substring(idx)
   return JSON.parse(json)
-}
-
-export const decodeST = (base64str) => {
-  return cbor.decode(decodeBase64(base64str))
 }
