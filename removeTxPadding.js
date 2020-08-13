a2ec6e91a365b1240d874d920b713ad5e1ddc5e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-13 11:30:35 +0000

    Deploy cloudwheels/dappforce-tendermint-explorer to github.com/cloudwheels/dappforce-tendermint-explorer.git:gh-pages

---
+++ removeTxPadding.js
@@ -0,0 +1,8 @@
+function removeTxPadding(input){
+    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+		var lkey = keyStr.indexOf(input.charAt(input.length - 1));
+		if(lkey == 64){
+			return input.substring(0,input.length - 1);
+		}
+		return input;
+	}
