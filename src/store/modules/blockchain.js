2f62e0dc51f8c94e0f54aa1aa63da9cdfa0a3b2a
strophy <badfunkstripe@gmail.com>
2021-02-15 18:26:04 -0800

    fix: don't use remote server for data

--- src/store/modules/blockchain.js
+++ src/store/modules/blockchain.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { RpcClient } from "tendermint"
 
 const state = {
-  rpc: "https://rpc.cloudwheels.net:26657",
+  rpc: "http://localhost:26657",
   //lcd: "https://gaia-seeds.interblock.io:1317",
   status: {
     listen_addr: "",
@@ -24,7 +24,7 @@ const state = {
   roundStep: ""
 }
 
-const client = RpcClient("wss://rpc.cloudwheels.net:26657")
+const client = RpcClient("wss://localhost:26657")
 
 const actions = {
   subNewBlock({ commit, dispatch }) {
